4969ca1fe52ffb6814beee6edb5fd7b99db5a6d6
Ziya SARIKAYA <sarikayaziya@gmail.com>
2013-11-10 20:20:49 +0200

    Fixed errors

--- src/Samples/PublishR.Mvc.ACommerce/Handlers/ProductOperationsHandler.ashx.cs
+++ src/Samples/PublishR.Mvc.ACommerce/Handlers/ProductOperationsHandler.ashx.cs
@@ -4,27 +4,24 @@ using PublishR.Messaging;
 using PublishR.Sample.MessageLibrary;
 using PublishR.Web.AspNet;
 
-namespace PublishR.Mvc.ACommerce.Handlers
-{
+namespace PublishR.Mvc.ACommerce.Handlers {
     public class ProductOperationsHandler : PublishrHandler,
         IHandle<ProductCreatedMessage>,
-        IHandle<ProductUpdatedMessage>
-    {
-        public void Handle(ProductCreatedMessage message)
-        {
+        IHandle<ProductUpdatedMessage>,
+        IHandle<ProductDeletedMessage> {
+
+        public void Handle(ProductCreatedMessage message) {
             //Get hub with hub name
             IHubContext hubContext = GlobalHost.ConnectionManager.GetHubContext(message.HubName);
             hubContext.Clients.All.productCreated(new { Message = "Product Create handled in ProductOperationsHandler.", message.ProductId });
         }
 
-        public void Handle(ProductUpdatedMessage message)
-        {
+        public void Handle(ProductUpdatedMessage message) {
             //Use current hub property
             CurrentHubContext.Clients.All.productUpdated(new { Message = "Product Update handled in ProductOperationsHandler.", message.ProductId });
         }
 
-        public void Handle(ProductDeletedMessage message)
-        {
+        public void Handle(ProductDeletedMessage message) {
             //Get hub with hub type
             IHubContext hubContext = GlobalHost.ConnectionManager.GetHubContext<PublishrHub>();
             hubContext.Clients.All.productDeleted(new { Message = "Product delete handled in ProductOperationsHandler.", message.ProductId });
